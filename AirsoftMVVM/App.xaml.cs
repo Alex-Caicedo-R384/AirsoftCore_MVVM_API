8220ec569953787915e1a334efd258e91e587a39
Alex Caiedo <alex.caicedo@udla.edu.ec>
2024-01-28 00:39:57 -0500

    Agregar archivos de proyecto.

---
+++ AirsoftMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AirsoftMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AirsoftMVVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AirsoftMVVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
